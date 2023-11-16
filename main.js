525bf1ec39799b5450a15445092edebdcb89452d
vitodursopettersson <vitodurso98@gmail.com>
2023-11-16 21:16:12 +0100

    push: roadmap

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 'use strict'
 
+// L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+// Ogni cella ha un numero progressivo, da 1 a 100.
+// Ci saranno quindi 10 caselle per ognuna delle 10 righe.
+// Quando l'utente clicca su ogni cella, 
+// la cella cliccata si colora di azzurro ed emetto un messaggio in console
+// con il numero della cella cliccata.
+
+// Tasto per azionare il gioco
+// Dropdown per scegliere la difficoltà
+// - in base alla difficoltà inesrire il numero di caselle da generare
+// Crezione della griglia
+// - Creare un elemnto div con una classe e un contenuto variabile
+// - Creare un ciclo per N volte della creazione dell'elemento e 
+//   aggiungere la classe in base alla difficoltà scelta (per la sua dimensione)
+// Quando clicco sul tasto Play prima resetta il campo di gioco
